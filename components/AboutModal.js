a481f9b4d18ebef4be26d91513ceaeaea37ac414
mani-barathi <manibarathi.s@gmail.com>
2021-03-16 22:08:01 +0530

    added transition for About Modal

--- components/AboutModal.js
+++ components/AboutModal.js
@@ -1,20 +1,30 @@
-import { useRef } from 'react'
-import styled from "styled-components"
+import { useEffect, useRef, useState } from 'react'
+import styled, { keyframes } from "styled-components"
 
 export default function AboutModal({ open, setOpen }) {
     const wrapperRef = useRef()
+    const [slideIn, setSlideIn] = useState(false)
+
+    useEffect(() => {
+        setSlideIn(open)
+    }, [open])
 
     if (!open)
         return null
 
     const closeModal = (e) => {
-        if (e.target === wrapperRef.current)
-            setOpen(false)
+        if (e.target === wrapperRef.current) {
+            setSlideIn(false)
+            let timer = setTimeout(() => {
+                setOpen(false)
+                clearTimeout(timer)
+            }, 500)
+        }
     }
 
     return (
         <Overlay ref={wrapperRef} onClick={closeModal}>
-            <Wrapper >
+            <Wrapper slideIn={slideIn} animation={slideDownAnimation} >
                 <h1>About Me</h1>
                 <Avatar src="https://cdn0.iconfinder.com/data/icons/avatar-78/128/12-512.png" alt="" />
                 <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos repellat eaque nemo incidunt tempora! Beatae deleniti impedit perspiciatis assumenda quidem aliquid praesentium sed quam ullam repellat obcaecati optio sunt quaerat, quod esse perferendis, rem suscipit? Ex soluta dignissimos quas ratione.</div>
@@ -34,8 +44,12 @@ const Overlay = styled.div`
     justify-content:center;
     align-items:center;
     background-color:rgba(0,0,0,0.5);
-    transition: all 0.3s ease;
+    transition: all 0.5s ease;
     padding: 1rem;
+`
+const slideDownAnimation = keyframes`
+  from{ transform: translatey(-5vh) ; opacity:0; }
+  to {transform: translatey(0) ; opacity:1; }
 `
 const Wrapper = styled.div`
     padding: 1rem;
@@ -49,6 +63,9 @@ const Wrapper = styled.div`
     justify-content:center;
     align-items:center;
     box-shadow: 0px 5px 16px 0px rgba(50, 50, 50, 0.73);
+    opacity:${props => props.slideIn ? '1' : '0'};
+    transform:${props => props.slideIn ? 'translatey(0)' : 'translatey(-30vh)'};
+    transition: all 0.3s ease;
 `
 const Avatar = styled.img`
     border-radius:50%;
