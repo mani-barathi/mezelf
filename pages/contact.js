586933a49433c06b272b6252fe334377fc3ee880
mani-barathi <manibarathi.s@gmail.com>
2021-03-18 20:15:02 +0530

    switching form fields

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,31 @@
-import { } from 'react'
-import styled, { css } from "styled-components"
+import { useRef, useState } from 'react'
+import styled from "styled-components"
 import { Form, FormGroup, FormInput } from "@/styles/Form"
 import { Text } from "@/styles/Typgraphy"
 import { Button } from "@/styles/Button"
 
+const KEYS = ['name', 'email', 'message']
 export default function contact() {
+    const [currentField, setCurrentField] = useState(0)
+    const [currentFieldValue, setCurrentFieldValue] = useState('')
+    const dataRef = useRef({})
+
+    const handleOnChange = (e) => setCurrentFieldValue(e.target.value)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dataRef.current[KEYS[currentField]] = currentFieldValue
+
+        console.log(dataRef.current)
+
+        if (currentField <= 1)
+            setCurrentField(prev => prev + 1)
+        else if (currentField === 2)
+            setCurrentField(0)
+
+        setCurrentFieldValue('')
+    }
+
     return (
         <Wrapper>
             <Container>
@@ -17,20 +38,26 @@ export default function contact() {
                     <br />
                     <br />
                 </div>
-                <Form autoComplete="off">
-                    <FormGroup>
-                        <label htmlFor="name">Name</label>
-                        <FormInput type="text" name="name" id="name" required />
-                    </FormGroup>
-                    <Button textBlue>Next</Button>
-                    {/* <FormGroup>
-                        <label htmlFor="email">Email</label>
-                        <FormInput type="text" name="email" id="email" required />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="email">Message</label>
-                        <FormInput as="textarea" rows="5" type="text" name="message" id="email" required />
-                    </FormGroup> */}
+                <Form onSubmit={handleSubmit} autoComplete="off">
+                    {(currentField === 0) &&
+                        <FormGroup>
+                            <label htmlFor="name">Name</label>
+                            <FormInput value={currentFieldValue} onChange={handleOnChange} type="text" name="name" id="name" required />
+                        </FormGroup>
+                    }
+                    {(currentField === 1) &&
+                        <FormGroup>
+                            <label htmlFor="email">Email</label>
+                            <FormInput value={currentFieldValue} onChange={handleOnChange} type="email" name="email" id="email" required autoFocus />
+                        </FormGroup>
+                    }
+                    {(currentField === 2) &&
+                        <FormGroup>
+                            <label htmlFor="email">Message</label>
+                            <FormInput value={currentFieldValue} onChange={handleOnChange} as="textarea" rows="5" type="text" name="message" id="email" required autoFocus />
+                        </FormGroup>
+                    }
+                    <Button textBlue>NEXT</Button>
                 </Form>
                 <div style={{ width: "100%", marginTop: "4rem" }} >
                     <Text medium>Let's Get Social</Text>
