3e85da811487ae802848cfb0d36953e4f9a18bc1
mani-barathi <manibarathi.s@gmail.com>
2021-03-19 22:44:51 +0530

    ContactForm design and functionaly

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,51 +1,57 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import { Form, FormGroup, FormInput } from "@/styles/Form"
 import { Button } from "@/styles/Button"
 import { fadeInAnimation } from "@/styles/Animation"
 
-const KEYS = ['name', 'email', 'message']
-
 export default function ContactForm() {
-    const [currentField, setCurrentField] = useState(0)
-    const [currentFieldValue, setCurrentFieldValue] = useState('')
-    const dataRef = useRef({})
+    const [currentField, setCurrentField] = useState(0)        // 0 - name Field, 1 - email Field, 2 - message Field
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
 
-    const handleOnChange = (e) => setCurrentFieldValue(e.target.value)
+    const goToPreviousField = () => {
+        setCurrentField(prev => prev - 1)
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        dataRef.current[KEYS[currentField]] = currentFieldValue
-
-        console.log(dataRef.current)
 
         if (currentField <= 1)
             setCurrentField(prev => prev + 1)
-        else if (currentField === 2)
+        else {
+            // Saving Form Response Goes Here.......
+            const data = { name, email, message }
+            console.log("data: ", data)
+            setName('')
+            setEmail('')
+            setMessage('')
             setCurrentField(0)
-
-        setCurrentFieldValue('')
+        }
     }
+
     return (
         <Form onSubmit={handleSubmit} autoComplete="off">
             {(currentField === 0) &&
                 <FormGroup animation={fadeInAnimation}>
                     <label htmlFor="name">Name</label>
-                    <FormInput value={currentFieldValue} onChange={handleOnChange} type="text" name="name" id="name" required />
+                    <FormInput value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" id="name" required autoFocus placeholder="John Doe" />
                 </FormGroup>
             }
             {(currentField === 1) &&
                 <FormGroup animation={fadeInAnimation}>
                     <label htmlFor="email">Email</label>
-                    <FormInput value={currentFieldValue} onChange={handleOnChange} type="email" name="email" id="email" required autoFocus />
+                    <FormInput value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" required autoFocus placeholder="john@email.com" />
                 </FormGroup>
             }
             {(currentField === 2) &&
                 <FormGroup animation={fadeInAnimation}>
                     <label htmlFor="email">Message</label>
-                    <FormInput value={currentFieldValue} onChange={handleOnChange} as="textarea" rows="4" type="text" name="message" id="email" required autoFocus />
+                    <FormInput value={message} onChange={(e) => setMessage(e.target.value)} as="textarea" rows="4" name="message" id="email" required autoFocus placeholder="Hey, your website is awesome!" />
                 </FormGroup>
             }
-            <Button textBlue>NEXT</Button>
+            <Button type="button" disabled={(currentField === 0)} onClick={goToPreviousField} >PREVIOUS</Button>
+            &nbsp; &nbsp;
+            <Button textBlue>{(currentField === 2) ? 'SEND' : 'NEXT'}</Button>
         </Form>
     )
 }
