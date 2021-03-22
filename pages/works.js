06c1b46862110c0099185fecd2f319120615aeed
mani-barathi <manibarathi.s@gmail.com>
2021-03-22 12:39:09 +0530

    layout of works page✔

--- pages/works.js
+++ pages/works.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { } from 'react'
+import styled from "styled-components"
+import { Text } from "@/styles/Typgraphy";
 
 export default function works() {
     return (
-        <h1>
-            Works Page
-        </h1>
+        <Wrapper>
+            <Container>
+                <Text large>My Works</Text>
+                <br />
+                <p>From Frontend to Backend to Serverless. Check out my latest Projects.</p>
+            </Container>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    flex:1;
+    padding:1rem;
+    display:flex;
+    justify-content:center;
+`
+const Container = styled.div`
+    width:100%;
+    max-width:800px;
+    display:flex;
+    flex-direction:column;
+`
