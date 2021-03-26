93f75f9fc64c45ed47c9a334ea9890c68c0ec50e
mani-barathi <manibarathi.s@gmail.com>
2021-03-26 12:55:23 +0530

    updated api route with Http method check to take only POST request

--- pages/api/message.js
+++ pages/api/message.js
@@ -2,6 +2,10 @@ const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 export default async (req, res) => {
+
+  if (req.method !== 'POST')
+    return res.status(403).json({ report: false, message: '403 Forbidden.' })
+
   const { name, email, message } = JSON.parse(req.body)
   const msg = {
     to: process.env.RECEIVER_EMAIL,   // Change to your recipient
